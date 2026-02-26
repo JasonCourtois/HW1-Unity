5b9af5ad5ebdc0f4e2e59ceaf39ba2a9b288559f
JasonCourtois <148589763+JasonCourtois@users.noreply.github.com>
2026-02-26 14:18:57 -0500

    Finished Assignment 1

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraControl : MonoBehaviour
+{
+    // Input Variables
+    private InputAction _rotateAction;
+    private InputAction _moveAction;
+    
+    // Speed Variables
+    [SerializeField, Range(0f, 100f)] private float _rotateSpeed = 50;
+    [SerializeField, Range(0f, 20f)] private float _moveSpeed = 7;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // For this assignment I chose to use the new input system as that is what I am currently using for my final project.
+        _rotateAction = InputSystem.actions.FindAction("Rotate");
+        _moveAction = InputSystem.actions.FindAction("Move");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotateValue = _rotateAction.ReadValue<float>();
+        Vector3 moveValue = _moveAction.ReadValue<Vector3>();
+
+        // I chose to use a vector 3 to also allow up and down movement with space and shift keys. This multiplication applies move speed and delta time to each value.
+        moveValue *= Time.deltaTime * _moveSpeed;
+
+        transform.Rotate(Vector3.up * _rotateSpeed * Time.deltaTime * rotateValue);
+        transform.Translate(moveValue);
+    }
+}
---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ChangeColor : MonoBehaviour
+{
+    // Input variables
+    private InputAction _startAction;
+    private InputAction _stopAction;
+
+    // Color variables
+    private Color[] _colors = new Color[] {Color.red, Color.blue, Color.green};
+    private int _newColorIndex = 0;
+    private Coroutine _colorCoroutine;  // Reference to coroutine stored to stop it when c is pressed.
+    [SerializeField, Range(0f, 1f)] private float _lerpRate = 0.2f;    // Rate colors will be transitioned at.
+
+    // Renderer used to access current material of object.
+    private Renderer _renderer;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _startAction = InputSystem.actions.FindAction("Start");
+        _stopAction = InputSystem.actions.FindAction("Stop");
+
+        _renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Only start if coroutine isn't already running.
+        if (_startAction.WasPressedThisFrame() && _colorCoroutine == null)
+        {
+            _colorCoroutine = StartCoroutine(ChangeColors());
+        }
+
+        // Similarly, only end coroutine if it is running.
+        if (_stopAction.WasPressedThisFrame() && _colorCoroutine != null)
+        {
+            StopCoroutine(_colorCoroutine);
+            _colorCoroutine = null;
+        }
+    }
+
+    private IEnumerator ChangeColors()
+    {
+        // Will continue changing colors until it is manually stopped
+        while (true)
+        {
+            float lerpProgress = 0;
+            Color oldColor = _renderer.material.color;
+            while (lerpProgress < 1)
+            {
+                Color lerpedColor = Color.Lerp(oldColor, _colors[_newColorIndex], lerpProgress);
+                lerpProgress += Time.deltaTime * _lerpRate;
+                _renderer.material.color = lerpedColor;
+                yield return null;
+            }
+            // Advance new color index and loop it to start if end was reached.
+            _newColorIndex = _newColorIndex == _colors.Length - 1 ? 0 : _newColorIndex + 1;
+
+            // Waits for 2 seconds until changing to next color as written in assignment instructions.
+            yield return new WaitForSeconds(2);
+        }
+    }
+
+}
